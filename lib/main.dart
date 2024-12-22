8a53f5722a33f715df85e4d33a510e8e0da85e2c
imane <akoudadimane2001@gmail.com>
2024-12-22 18:05:51 +0100

    Premier commit - Ajout de l'application

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_list/auth/main_page.dart';
+import 'package:flutter_to_do_list/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Déconnecter l'utilisateur à chaque démarrage
+  await FirebaseAuth.instance.signOut();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Main_Page(),
+    );
+  }
+}
